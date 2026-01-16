a2d6bf280c920dadd2bba64c243f711d1311061d
fhdl1221 <fhdl353212@gmail.com>
2026-01-16 09:14:17 +0900

    first commit

---
+++ src/test/java/com/example/gallery_back/GalleryBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gallery_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
